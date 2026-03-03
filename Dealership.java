9a960f7f2850ab87cab5b01c72602384ff9a1975
Jonathan Leohr <40473237+jleohr@users.noreply.github.com>
2026-03-03 13:47:12 -0600

    Create Dealership.java

---
+++ Dealership.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+
+public class Dealership {
+    public static void main(String[] args) {
+        ArrayList<Vehicle> inventory = new ArrayList<Vehicle>();
+
+        // TODO: Add a standard Vehicle and an ElectricCar
+        
+        System.out.println("--- End of Day Report ---");
+        // TODO: Loop through inventory. 
+        // TODO: If a vehicle is an ElectricCar, check if it needs a charge.
+    }
+}
